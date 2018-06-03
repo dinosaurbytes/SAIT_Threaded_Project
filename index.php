78a02c40e0f098af3fee231f15c534e90a90921d
Sunghyun Lee <773646@ACDM.DS.SAIT.CA>
2018-06-03 14:51:21 -0600

    Sung
    index.php: added search box and popular packages
    
    register.php: added register form

--- index.php
+++ index.php
@@ -5,80 +5,16 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/SungCss.css">
 </head>
 <body>
   <div class="container">
 
-    <?php include "php/header.php"?>
+    <?php include "php/header.php";
+          include "php/searchAndPackage.php";
+    ?>
 
-    <div class='jumbotron' style='background-image: url("image/bgimg.jpeg")'>
-      <h1 class='display-4'>simple</h1>
-      <p class ="lead">wjpajdpajdpoa wdjawpojd apowjd poawj dpoaj wd wadjpoaw </p>
-      <p class ="lead">sdafsdafsdafsadfadsfs</p>
-    </div>
-<!-- Row 1 -->
-    <div class="row">
-      <div class='col-xs-12 col-md'>
-        <div class='card mb-4'>
-          <div class='card-body text-center'>
-            <h5 class='card-title'>1</h5>
-            <p class="card-text">some text</p>
-            <a href="#" class='card-link'>another link</a>
-          </div>
-        </div>
-      </div>
-      <div class='col-xs-12 col-md'>
-        <div class='card mb-4'>
-          <div class='card-body text-center'>
-            <h5 class='card-title'>2</h5>
-            <p class="card-text">some text</p>
-            <a href="#" class='card-link'>another link</a>
-          </div>
-        </div>
-      </div>
-      <div class='col-xs-12 col-md'>
-        <div class='card mb-4'>
-          <div class='card-body text-center'>
-            <h5 class='card-title'>3</h5>
-            <p class="card-text">some text</p>
-            <a href="#" class='card-link'>another link</a>
-          </div>
-        </div>
-      </div>
-    </div>
-<!-- row 2 -->
-    <div class="row">
-      <div class='col-xs-12 col-md'>
-        <div class='card mb-4'>
-          <div class='card-body text-center'>
-            <h5 class='card-title'>4</h5>
-            <p class="card-text">some text</p>
-            <a href="#" class='card-link'>another link</a>
-          </div>
-        </div>
-      </div>
-      <div class='col-xs-12 col-md'>
-        <div class='card mb-4'>
-          <div class='card-body text-center'>
-            <h5 class='card-title'>5</h5>
-            <p class="card-text">some text</p>
-            <a href="#" class='card-link'>another link</a>
-          </div>
-        </div>
-      </div>
-      <div class='col-xs-12 col-md'>
-        <div class='card mb-4'>
-          <div class='card-body text-center'>
-            <h5 class='card-title'>6</h5>
-            <p class="card-text">some text</p>
-            <a href="#" class='card-link'>another link</a>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-
+    
   </div>
 
 <?php include "php/footer.php"?>
