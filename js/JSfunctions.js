2355f021584810758372b46437b500002ba8c1da
Sunghyun Lee <773646@ACDM.DS.SAIT.CA>
2018-06-05 23:40:09 -0600

    JSfunctions.js and classes.php
    created seperate files for javascript functions and php classes.
    
    Modified packages.php page to display the data from the database

---
+++ js/JSfunctions.js
@@ -0,0 +1,47 @@
+/*
+I'm still not too sure about the file heading format. If you guys have any ideas, please feel free to 
+modify this heading. Also go ahead and put your js functions on this file!
+
+
+
+Author: Fantastic 4 (Jason, Brian, Lindsay, Sunghyun)
+Description: this is where we define Custom javascript functions
+Created: 2018-06-05
+
+
+*/
+
+
+
+
+function display_destination() 
+{
+  /**
+    This function will only display row of the searched destination.
+    Author: Sunghyun Lee
+  */
+
+  // Declare variables
+  var myInput = document.getElementById("searchInput");
+  var upperInputValue= myInput.value.toUpperCase();
+  var myTable = document.getElementById("packageTable");
+  var rows = myTable.getElementsByTagName("tr");
+
+  // Loop through rows and loop through td's. If there is any match, display the row. Otherwise, hide.
+  
+  for (var i = 0; i < rows.length; i++) 
+  { var match = false;
+    var datas = rows[i].getElementsByTagName("td");
+    for (var j=0; j<datas.length;j++)
+    { 
+      if (datas[j].innerHTML.toUpperCase().indexOf(upperInputValue) > -1) 
+        match=true;      
+    }
+    if (match)
+      rows[i].style.display = "";
+    else
+      rows[i].style.display = "none";
+
+  }
+  // end of for loop
+}
---
+++ php/classes.php
@@ -0,0 +1,35 @@
+<?php
+/**
+This is a php file where classes are defined
+Created: 2018-06-05
+*/
+
+class Package
+{	public $id;
+	public $name;
+	public $startDate;
+	public $endDate;
+	public $description;
+	public $basePrice;
+	public $agencyCommission;
+	public function __construct($pkgId)
+	{
+		$this->id = $pkgId;
+		$myDb = new mysqli('localhost', 'root', '','travelexperts');
+    	$sql = "SELECT PkgName,PkgStartDate, PkgEndDate,PkgDesc, PkgBasePrice, PkgAgencyCommission FROM packages WHERE PackageId = $pkgId";
+    	$result=$myDb->query($sql);
+    	$row = $result->fetch_row();
+    	$this->name=$row[0];
+    	$this->startDate =$row[1];
+    	$this->endDate=$row[2];
+    	$this->description=$row[3];
+    	$this->basePrice=$row[4];
+    	$this->agencyCommission=$row[5];
+    	$myDb->close();
+
+	}
+}
+
+
+
+?>
